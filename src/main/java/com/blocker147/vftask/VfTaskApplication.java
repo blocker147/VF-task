bc0a3a1439569a11a2fda556367262b448f38e4f
ilja.tarasovs <ilja.tarasovs@gmail.com>
2020-10-14 15:24:39 +0300

    Initial commit

---
+++ src/main/java/com/blocker147/vftask/VfTaskApplication.java
@@ -0,0 +1,13 @@
+package com.blocker147.vftask;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VfTaskApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VfTaskApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/blocker147/vftask/VfTaskApplicationTests.java
@@ -0,0 +1,13 @@
+package com.blocker147.vftask;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VfTaskApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
